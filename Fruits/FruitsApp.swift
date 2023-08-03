a4f67b8523707d1b2332b09f6f1ab730eb0ffa48
eng-oday <odaydieg@gmail.com>
2023-08-03 13:56:41 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by 3rabApp-oday on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
